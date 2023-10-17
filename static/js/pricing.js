50682a4753b10cf75bde149deb7ca89f38dfbcbd
tomesri <139428045+tomesri@users.noreply.github.com>
2023-10-17 16:19:24 +0800

    yes

--- static/js/pricing.js
+++
@@ -1,9 +0,0 @@
-$(document).on('mouseover', '.current-plan', function () {
-    $(this).find('span').text(trans('Renew Now'));
-    $(this).find('i').removeClass('fi-rr-badge-check').addClass('fi-rr-refresh');
-});
-
-$(document).on('mouseout', '.current-plan', function () {
-    $(this).find('span').text(trans('Current Plan'));
-    $(this).find('i').removeClass('fi-rr-refresh').addClass('fi-rr-badge-check');
-})
